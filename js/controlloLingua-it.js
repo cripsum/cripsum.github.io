ca56bc313333365bd01c6e50afba71d65ba04564
cripsum <galli.leonardo2005gmail.com>
2025-03-05 18:25:52 +0100

    fix: update language redirection logic to handle path adjustments for English and Italian

--- js/controlloLingua-it.js
+++ js/controlloLingua-it.js
@@ -14,6 +14,10 @@ if (selectedLanguage === "🇬🇧 Eng") {
     const newPath = pathParts.includes("en") ? pathParts.slice(0, -1).join("/") : pathParts.join("/");
     window.location.href = newPath + "/en/" + pageName;
 } else if (selectedLanguage === "🇮🇹 Ita") {
+    const pathParts = window.location.pathname.split("/");
+    const pageName = pathParts.pop();
+    const newPath = pathParts.includes("it") ? pathParts.slice(0, -1).join("/") : pathParts.join("/");
+    window.location.href = newPath + "/it/" + pageName;
 }
 
 
