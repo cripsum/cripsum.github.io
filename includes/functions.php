8d67991d8f74192f1bfc3fac151b99e3a6287b2b
cripsum <galli.leonardo2005gmail.com>
2025-07-05 03:59:25 +0200

    fix: remove error reporting from database and navbar files

--- includes/functions.php
+++ includes/functions.php
@@ -1,7 +1,4 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
 
 function loginUser($mysqli, $email, $password) {
     $stmt = $mysqli->prepare("SELECT id, username, email, password, profile_pic FROM utenti WHERE email = ?");
