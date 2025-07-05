8523ba96307e313a11ebac6884dbe967a719a869
cripsum <galli.leonardo2005gmail.com>
2025-07-05 16:46:54 +0200

    fix: update file paths in user.php for correct resource loading

--- user.php
+++ user.php
@@ -94,28 +94,121 @@ if ($is_own_profile && $_SERVER['REQUEST_METHOD'] === 'POST' && ($_POST['action'
             integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
             crossorigin="anonymous"
         />
-        <link rel="icon" href="img/Susremaster.png" type="image/png" />
-        <link rel="shortcut icon" href="img/Susremaster.png" type="image/png" />
+        <link rel="icon" href="../img/Susremaster.png" type="image/png" />
+        <link rel="shortcut icon" href="../img/Susremaster.png" type="image/png" />
         <link href="https://fonts.googleapis.com/css?family=Poppins" rel="stylesheet" />
-        <link rel="stylesheet" href="css/style.css" />
-        <link rel="stylesheet" href="css/style-dark.css" />
-        <link rel="stylesheet" href="css/animations.css" />
-        <link rel="stylesheet" href="css/achievement-style.css" />
+        <link rel="stylesheet" href="../css/style.css" />
+        <link rel="stylesheet" href="../css/style-dark.css" />
+        <link rel="stylesheet" href="../css/animations.css" />
+        <link rel="stylesheet" href="../css/achievement-style.css" />
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-        <script src="js/animations.js"></script>
+        <script src="../js/animations.js"></script>
 
-        <script src="js/controlloLingua-it.js"></script>
-        <script src="js/controlloTema.js"></script>
-        <script src="js/unlockAchievement-it.js"></script>
-        <script src="js/achievements-globali.js"></script>
-        <script src="js/richpresence.js"></script>
+        <script src="../js/controlloLingua-it.js"></script>
+        <script src="../js/controlloTema.js"></script>
+        <script src="../js/unlockAchievement-it.js"></script>
+        <script src="../js/achievements-globali.js"></script>
+        <script src="../js/richpresence.js"></script>
 
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Cripsum™ - Profilo</title>
 </head>
 <body >
 
-    <?php include 'includes/navbar.php'; ?>
+    <?php
+session_start();
+$isLoggedIn = isset($_SESSION['user_id']) && !empty($_SESSION['user_id']);
+
+if ($isLoggedIn) {
+    $username = $_SESSION['username'] ?? 'Utente';
+    $userId = $_SESSION['user_id'];
+    $profilePic = "/includes/get_pfp.php?id=$userId";
+    $ruolo = $_SESSION['ruolo'] ?? '';
+}
+?>
+
+<nav class="navbarutenti navbar navbar-expand-xl fadein">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="home">
+            <img src="../img/amongus.jpg" height="40px" style="border-radius: 4px" class="d-inline-block align-middle" />
+            <span class="align-middle ms-3 fw-bold testobianco">Cripsum™</span>
+        </a>
+
+        <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+            style="z-index: 1000"
+        >
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item"><a class="nav-link" href="../it/home">Home page</a></li>
+                <li class="nav-item dropdown dropdownutenti">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">Memes</a>
+                    <ul class="dropdown-menu animate slideIn">
+                        <li><a class="dropdown-item" href="../it/shitpost">Shitpost</a></li>
+                        <li><a class="dropdown-item" href="../it/tiktokpedia">TikTokPedia</a></li>
+                        <li><a class="dropdown-item" href="../it/rimasti">Top rimasti</a></li>
+                        <li><a class="dropdown-item" href="../it/quandel57" style="color: red; font-weight: bold">Quandel57</a></li>
+                        <li><a class="dropdown-item arcobalenos" href="../it/gambling" style="font-weight: bold">Gambling!!</a></li>
+                        <li><a class="dropdown-item testo-arcobaleno" href="../it/lootbox" style="font-weight: bold">Lootbox</a></li>
+                        <li><a class="dropdown-item" href="../it/achievements">Achievements</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item"><a class="nav-link" href="../it/download">Downloads</a></li>
+                <li class="nav-item dropdown dropdownutenti">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">Shop</a>
+                    <ul class="dropdown-menu animate slideIn">
+                        <li><a class="dropdown-item" href="../it/negozio">Negozio</a></li>
+                        <li><a class="dropdown-item" href="../it/merch">Merch</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item"><a class="nav-link" href="../it/donazioni">Donazioni</a></li>
+                <li class="nav-item"><a class="nav-link" href="../it/chisiamo">Chi siamo</a></li>
+                <li class="nav-item"><a class="nav-link" href="../it/edits">Edits</a></li>
+            </ul>
+
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                <?php if (!$isLoggedIn): ?>
+                    <li class="nav-item"><a class="nav-link" href="../it/accedi">Accedi</a></li>
+                    <li class="nav-item"><a class="nav-link" href="../it/registrati">Registrati</a></li>
+                <?php else: ?>
+                    <li class="nav-item dropdown dropdownutenti">
+                        <a class="nav-link dropdown-toggle d-flex align-items-center" href="#" role="button" data-bs-toggle="dropdown">
+                            <img src="<?php echo htmlspecialchars($profilePic); ?>" alt="../it/Profilo" 
+                                 class="rounded-circle me-2" style="width: 30px; height: 30px; object-fit: cover;">
+                            <span><?php echo htmlspecialchars($username); ?></span>
+                        </a>
+                        <ul class="dropdown-menu dropdown-menu-end animate slideIn">
+                            <li><a class="dropdown-item" href="../it/profilo"><i class="fas fa-user me-2"></i>Il mio profilo</a></li>
+                            <li><a class="dropdown-item" href="../it/impostazioni"><i class="fas fa-cog me-2"></i>Impostazioni</a></li>
+                            <li><a class="dropdown-item" href="../it/ordini"><i class="fas fa-shopping-bag me-2"></i>I miei ordini</a></li>
+                            <?php if ($ruolo === 'admin'): ?>
+                                <li><a class="dropdown-item" href="../it/admin"><i class="fas fa-shield-alt me-2"></i>Pannello Admin</a></li>
+                            <?php endif; ?>
+                            <li><hr class="dropdown-divider"></li>
+                            <li><a class="dropdown-item text-danger" href="https://cripsum.com/logout"><i class="fas fa-sign-out-alt me-2"></i>Logout</a></li>
+                        </ul>
+                    </li>
+                <?php endif; ?>
+            </ul>
+        </div>
+
+        <div class="btn-group ms-auto me-3 linguanuova">
+            <button type="button" class="btn impostazioni-toggler" data-bs-toggle="modal" data-bs-target="#impostazioniModal">
+                <img src="../img/settings-icon.svg" alt="Impostazioni" style="width: 25px" class="imgbianca" />
+            </button>
+        </div>
+    </div>
+</nav>
+
 
     <div class="container my-5 paginainterachisiamo testobianco" style="padding-top: 7rem">
         <h1 class="mb-4 fadeup">Profilo di <?php echo htmlspecialchars($user['username']); ?></h1>
@@ -172,7 +265,7 @@ if ($is_own_profile && $_SERVER['REQUEST_METHOD'] === 'POST' && ($_POST['action'
         <?php endif; ?>
 
         <div class="mt-4 fadeup">
-            <a href="home.php" class="linkbianco">← Torna alla home</a>
+            <a href="../it/home.php" class="linkbianco">← Torna alla home</a>
         </div>
     </div>
 
@@ -188,9 +281,9 @@ if ($is_own_profile && $_SERVER['REQUEST_METHOD'] === 'POST' && ($_POST['action'
         <footer class="my-5 pt-5 text-muted text-center text-small fadeup">
             <p class="mb-1 testobianco">Copyright © 2021-2025 Cripsum™. Tutti i diritti riservati.</p>
             <ul class="list-inline">
-                <li class="list-inline-item"><a href="privacy" class="linkbianco">Privacy</a></li>
-                <li class="list-inline-item"><a href="tos" class="linkbianco">Termini</a></li>
-                <li class="list-inline-item"><a href="supporto" class="linkbianco">Supporto</a></li>
+                <li class="list-inline-item"><a href="../it/privacy" class="linkbianco">Privacy</a></li>
+                <li class="list-inline-item"><a href="../it/tos" class="linkbianco">Termini</a></li>
+                <li class="list-inline-item"><a href="../it/supporto" class="linkbianco">Supporto</a></li>
             </ul>
         </footer>
         <script
@@ -199,6 +292,6 @@ if ($is_own_profile && $_SERVER['REQUEST_METHOD'] === 'POST' && ($_POST['action'
             crossorigin="anonymous"
         ></script>
 
-        <script src="js/modeChanger.js"></script>
+        <script src="../js/modeChanger.js"></script>
 </body>
 </html>
