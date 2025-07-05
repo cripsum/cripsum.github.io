ae2e89b04e7fe295a36eb2009ab419eaf0035f55
cripsum <galli.leonardo2005gmail.com>
2025-07-05 16:35:10 +0200

    fix: enhance error reporting in user and profile pages

--- user/profilo.php
+++ user/profilo.php
@@ -1,4 +1,10 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+ini_set('log_errors', 1);
+ini_set('error_log', __DIR__ . '/error.log');
+error_reporting(E_ALL);
+
 session_start();
 require_once '../config/database.php';
 require_once '../includes/functions.php';
