e3b0af7cff867026adc07191754fad01802ed851
cripsum <galli.leonardo2005gmail.com>
2025-07-05 03:08:47 +0200

    fix: update database connection parameters and require path

--- config/database.php
+++ config/database.php
@@ -1,11 +1,10 @@
 <?php
-require_once __DIR__ . '/../secure/config.php';
+require_once '../secure/config.php';
 
-$mysqli = new mysqli($host, $username, $password, $database);
+$mysqli = new mysqli($db_host, $db_user, $db_pass, $db_name);
 
 if ($mysqli->connect_error) {
     die('Connection failed: ' . $mysqli->connect_error);
 }
 
-$mysqli->set_charset('utf8');
 ?>
