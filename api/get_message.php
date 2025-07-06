fd5d7c6ce8d0f85c540a80d780e78512e8396894
cripsum <galli.leonardo2005gmail.com>
2025-07-06 21:01:19 +0200

    feat: enable error reporting in chat-related PHP files

--- api/get_message.php
+++ api/get_message.php
@@ -1,4 +1,10 @@
 <?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+ini_set('log_errors', 1);
+error_reporting(E_ALL);
+
 require_once __DIR__ . '/../config/database.php';
 require_once __DIR__ . '/../config/chat_config.php';
 require_once __DIR__ . '/../includes/functions.php';
