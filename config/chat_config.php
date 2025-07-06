05d89b2e5f16beec817861208acabb4e890ed626
cripsum <galli.leonardo2005gmail.com>
2025-07-06 23:53:19 +0200

    fix: increase maximum message length from 30 to 300 for better user experience

--- config/chat_config.php
+++ config/chat_config.php
@@ -1,5 +1,5 @@
 <?php
-define('MAX_MESSAGE_LENGTH', 30);
+define('MAX_MESSAGE_LENGTH', 300);
 define('MESSAGE_TIMEOUT', 5); // in seconds
 define('USER_ROLE_ADMIN', 'admin');
 define('USER_ROLE_USER', 'user');
