bab7137eee958cb996140fa6f52119da18c4c5d5
cripsum <galli.leonardo2005gmail.com>
2025-07-25 23:08:49 +0200

    fix: add error reporting settings to incrementa_counter_goon.php

--- api/incrementa_counter_goon.php
+++ api/incrementa_counter_goon.php
@@ -1,4 +1,9 @@
 <?php  
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', value: 1);
+ini_set('log_errors', 1);
+error_reporting(1);
+
 require_once __DIR__ . '/../config/database.php';
 require_once __DIR__ . '/../includes/functions.php';
 
