017d1a8f32885f113b69f2c69c9a96ebbde57d68
cripsum <galli.leonardo2005gmail.com>
2025-07-25 23:41:57 +0200

    fix: correct require paths in incrementa_counter_goon.php for proper file inclusion

--- api/incrementa_counter_goon.php
+++ api/incrementa_counter_goon.php
@@ -4,8 +4,8 @@ ini_set('display_startup_errors', value: 1);
 ini_set('log_errors', 1);
 error_reporting(1);
 
-require_once __DIR__ . '/../../config/database.php';
-require_once __DIR__ . '/../../includes/functions.php';
+require_once __DIR__ . '/../config/database.php';
+require_once __DIR__ . '/../includes/functions.php';
 
 session_start();
 $user_id = $_SESSION['user_id'] ?? 0;
