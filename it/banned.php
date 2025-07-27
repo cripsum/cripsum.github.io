42819c63dae193d4c26999b6ed928059a091b4c6
cripsum <galli.leonardo2005gmail.com>
2025-07-27 07:16:40 +0200

    Implement code changes to enhance functionality and improve performance

--- it/banned.php
+++ it/banned.php
@@ -127,7 +127,7 @@ if(!isset($_COOKIE['banned']) || $_COOKIE['banned'] == '0') {
         <h1>Account Bannato</h1>
         <p>Il tuo account è stato sospeso per violazione dei nostri termini di servizio.</p>
         <p>Godo coglione</p>
-        <img src="/img/Laughing_emoji_no_background.webp" alt="">
+        <img src="/img/toppng.com-laughing-pointing-emoji-1645x1070.png" alt="">
         <p>Se ritieni che questo sia un errore, puoi contattare il nostro supporto.</p>
         
         <div class="contact-info">
