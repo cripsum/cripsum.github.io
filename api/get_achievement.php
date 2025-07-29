4c39850bc64d1494f0567848b3369e398ceefe6e
cripsum <galli.leonardo2005gmail.com>
2025-07-29 05:44:09 +0200

    fix: update SQL query to select all fields from achievement table

--- api/get_achievement.php
+++ api/get_achievement.php
@@ -5,7 +5,7 @@ require_once __DIR__ . '/../includes/functions.php';
 session_start();
 $achievement_id = $_GET['achievement_id'] ?? 0;
 
-    $stmt = $mysqli->prepare("SELECT id, nome, descrizione, punti, img_url FROM achievement WHERE achievement.id = ?"); 
+    $stmt = $mysqli->prepare("SELECT * FROM achievement WHERE achievement.id = ?"); 
     $stmt->bind_param("i", $achievement_id);
     $stmt->execute();
     $result = $stmt->get_result();
