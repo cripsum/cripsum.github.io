2d0b566f424c9d476c92bb8a82967b6ab97355cc
cripsum <galli.leonardo2005gmail.com>
2025-07-30 10:11:33 +0200

    fix: correct variable binding for user ID in achievement and character retrieval APIs

--- api/get_unlocked_achievement_from_id.php
+++ api/get_unlocked_achievement_from_id.php
@@ -6,7 +6,7 @@ session_start();
 $id = intval($_GET['id']);
 
     $stmt = $mysqli->prepare("SELECT id, nome, descrizione, punti, img_url, data FROM achievement, utenti_achievement WHERE achievement.id = utenti_achievement.achievement_id AND utenti_achievement.utente_id = ?"); 
-    $stmt->bind_param("i", $user_id);
+    $stmt->bind_param("i", $id);
     $stmt->execute();
     $result = $stmt->get_result();
     
--- api/get_unlocked_characters_from_id.php
+++ api/get_unlocked_characters_from_id.php
@@ -6,7 +6,7 @@ session_start();
 $id = intval($_GET['id']);
 
     $stmt = $mysqli->prepare("SELECT id, nome, rarità, categoria, img_url, audio_url, data, quantità FROM personaggi, utenti_personaggi WHERE personaggi.id = utenti_personaggi.personaggio_id AND utenti_personaggi.utente_id = ?"); 
-    $stmt->bind_param("i", $user_id);
+    $stmt->bind_param("i", $id);
     $stmt->execute();
     $result = $stmt->get_result();
     
