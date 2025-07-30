87f3adddc126812cad20268bab59ccca082c9ede
cripsum <galli.leonardo2005gmail.com>
2025-07-30 02:04:00 +0200

    feat: add logic to update user currency upon achievement completion

--- api/set_achievement.php
+++ api/set_achievement.php
@@ -23,6 +23,22 @@ if ($stmt->execute()) {
 }
 $stmt->close();
 
+$stmt2 = $mysqli->prepare("SELECT punti FROM achievement WHERE id = ?");
+$stmt2->bind_param("i", $achievement_id);
+
+if ($stmt2->execute()) {
+    $result2 = $stmt2->get_result();
+    if ($row2 = $result2->fetch_assoc()) {
+        $punti = (int)$row2['punti'];
+        $stmt3 = $mysqli->prepare("UPDATE utenti SET soldi = soldi + ? WHERE id = ?");
+        $stmt3->bind_param("ii", $punti, $user_id);
+        $stmt3->execute();
+        $stmt3->close();
+    }
+}
+$stmt2->close();
+
+
 header('Content-Type: application/json');
 echo json_encode($response);
 
