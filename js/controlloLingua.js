07c07501301f9a07aeb9349edcb5fd94af00b11f
cripsum <galli.leonardo2005gmail.com>
2025-07-30 20:29:53 +0200

    refactor: remove cookie handling for language selection and update redirection logic

--- js/controlloLingua.js
+++ js/controlloLingua.js
@@ -1,21 +1,3 @@
-function getCookie(name) {
-    const cookies = document.cookie.split("; ");
-    for (let cookie of cookies) {
-        let [key, value] = cookie.split("=");
-        if (key === name) return JSON.parse(value);
-    }
-    return null;
-}
-
-const selectedLanguage = getCookie("language") || "🇬🇧 Eng";
-if (selectedLanguage === "🇬🇧 Eng") {
-    
-} else if (selectedLanguage === "🇮🇹 Ita") {
-     const pageName = window.location.pathname.split("/").pop();
-    window.location.href = "https://cripsum.com/" + pageName;
-} 
-
-
 function salvaImpostazioni() {
     const selectedLanguage = document.querySelector(".selezione-lingua select").value;
 
