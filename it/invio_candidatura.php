a3e85ff9ee33e0f63c8d5b19ed360191081c15cf
cripsum <galli.leonardo2005gmail.com>
2025-08-13 01:16:43 +0200

    email candidatura fixata, forse

--- it/invio_candidatura.php
+++ it/invio_candidatura.php
@@ -8,7 +8,6 @@ require_once '../includes/functions.php';
 if(!isLoggedIn()) {
     $_SESSION['redirect_after_login'] = $_SERVER['REQUEST_URI'];
     $_SESSION['login_message'] = "Per poter mandare la tua candidatura devi avere un account Cripsum™";
-
     header('Location: accedi');
     exit();
 }
@@ -17,31 +16,32 @@ $user_id = $_SESSION['user_id'];
 $email = $_SESSION['email'] ?? '';
 $username = $_SESSION['username'];
 $userRole = $_SESSION['ruolo'] ?? 'utente';
-$profilePic = "/includes/get_pfp.php?id=$user_id";
 
 $username_chisiamo = $_POST['username'] ?? '';
 $email_chisiamo = $_POST['email'] ?? '';
 $descrizione_chisiamo = $_POST['descrizione'] ?? '';
-$pfp_chisiamo = '';
-$pfp_uploaded = false;
 $username_social = $_POST['social_username'] ?? '';
 $link_social = $_POST['social_link'] ?? '';
 
-
-$to = 'dio.covid@gmail.com';
-$subject = 'Nuova Candidatura - ' . $username_chisiamo;
+$pfp_chisiamo = '';
+$mime_type = '';
+$pfp_uploaded = false;
 
 if (isset($_FILES['pfp_chisiamo']) && $_FILES['pfp_chisiamo']['error'] === UPLOAD_ERR_OK && $_FILES['pfp_chisiamo']['size'] > 0) {
-    $allowed_types = ['image/jpeg', 'image/jpg', 'image/png', 'image/webp'];
     $finfo = new finfo(FILEINFO_MIME_TYPE);
     $mime_type = $finfo->file($_FILES['pfp_chisiamo']['tmp_name']);
     
+    $allowed_types = ['image/jpeg', 'image/png', 'image/webp'];
     if (in_array($mime_type, $allowed_types)) {
         $pfp_chisiamo = file_get_contents($_FILES['pfp_chisiamo']['tmp_name']);
         $pfp_chisiamo = base64_encode($pfp_chisiamo);
         $pfp_uploaded = true;
     }
 }
+
+$to = 'dio.covid@gmail.com';
+$subject = 'Nuova Candidatura - ' . $username_chisiamo;
+$boundary = md5(time());
 
 $message = "Nuova candidatura ricevuta:\n\n";
 $message .= "Username: " . $username_chisiamo . "\n";
@@ -58,7 +58,43 @@ $message .= "Username social: " . $username_social . "\n";
 $message .= "Link social: " . $link_social . "\n";
 $message .= "\nInviata da utente ID: " . $user_id . " (" . $username . " - " . $email  . ")";
 
-if(mail($to, $subject, $message, $headers)) {
+$headers = "From: noreply@cripsum.com\r\n";
+$headers .= "Reply-To: " . $email_chisiamo . "\r\n";
+$headers .= "MIME-Version: 1.0\r\n";
+$headers .= "Content-Type: multipart/mixed; boundary=\"{$boundary}\"\r\n";
+
+$email_message = "--{$boundary}\r\n";
+$email_message .= "Content-Type: text/plain; charset=UTF-8\r\n";
+$email_message .= "Content-Transfer-Encoding: 7bit\r\n\r\n";
+$email_message .= $message . "\r\n\r\n";
+
+if ($pfp_uploaded) {
+    $filename = 'profile_picture';
+    switch ($mime_type) {
+        case 'image/jpeg':
+            $filename .= '.jpg';
+            break;
+        case 'image/png':
+            $filename .= '.png';
+            break;
+        case 'image/webp':
+            $filename .= '.webp';
+            break;
+        default:
+            $filename .= '.jpg';
+            break;
+    }
+    
+    $email_message .= "--{$boundary}\r\n";
+    $email_message .= "Content-Type: {$mime_type}; name=\"{$filename}\"\r\n";
+    $email_message .= "Content-Transfer-Encoding: base64\r\n";
+    $email_message .= "Content-Disposition: attachment; filename=\"{$filename}\"\r\n\r\n";
+    $email_message .= chunk_split($pfp_chisiamo) . "\r\n";
+}
+
+$email_message .= "--{$boundary}--\r\n";
+
+if(mail($to, $subject, $email_message, $headers)) {
     $_SESSION['result_candidatura'] = "Candidatura inviata con successo!";
 } else {
     $_SESSION['result_candidatura'] = "Errore nell'invio della candidatura.";
@@ -66,5 +102,4 @@ if(mail($to, $subject, $message, $headers)) {
 
 header('Location: ' . $_SERVER['HTTP_REFERER']);
 exit();
-
 ?>
