5ede5e0bff3f2a3975ff94c8bb0f1843a49523a0
cripsum <galli.leonardo2005gmail.com>
2025-08-13 05:55:18 +0200

    gay

--- it/chisiamo.php
+++ it/chisiamo.php
@@ -48,7 +48,7 @@ checkBan($mysqli);
         <div class="floating-element"></div>
     </div>
 
-    <div class="main-container" style="padding-top: 2rem">
+    <div class="main-container fadeup" style="padding-top: 2rem">
         <section class="chisiamo-section">
             <h1 class="chisiamo-title">Il Nostro Team di Sviluppo</h1>
             <p class="chisiamo-subtitle">
