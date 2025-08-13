df2c26fcbc8ef1e0e8d275c63207942263c31252
cripsum <galli.leonardo2005gmail.com>
2025-08-13 05:36:21 +0200

    Remove Bootstrap CSS link from edits and candidatura pages; add Bootstrap JS link to improve functionality

--- it/candidatura-chisiamo.php
+++ it/candidatura-chisiamo.php
@@ -26,7 +26,6 @@ $result_candidatura = $_SESSION['result_candidatura'] ?? '';
 <html lang="it">
 <head>
     <?php include '../includes/head-import.php'; ?>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <meta charset="UTF-8">
     <title>Cripsum™ - Candidatura Chi siamo</title>
     <style>
@@ -96,5 +95,10 @@ $result_candidatura = $_SESSION['result_candidatura'] ?? '';
         </form>
         <a class="nav-link" href="../"><i class="fas fa-arrow-left"></i> Torna alla home</a>
     </div>
+    <script
+            src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+            crossorigin="anonymous"
+        ></script>
 </body>
 </html>
--- it/edits.php
+++ it/edits.php
@@ -368,8 +368,6 @@ checkBan($mysqli);
         }
         </style>
         <?php include '../includes/head-import.php'; ?>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-        <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet">
         <title>Cripsum™ - edits</title>
     </head>
     <body>
@@ -1095,7 +1093,6 @@ checkBan($mysqli);
 
     <?php include '../includes/footer.php'; ?>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
     <script>
             document.addEventListener('DOMContentLoaded', function() {
                 document.querySelectorAll('.filter-btn').forEach(btn => {
