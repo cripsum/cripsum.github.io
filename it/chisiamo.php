ca963bdc569fcd3d416eec6bd7394a9940669e67
cripsum <galli.leonardo2005gmail.com>
2025-08-14 13:01:56 +0200

    fix typo

--- it/chisiamo.php
+++ it/chisiamo.php
@@ -157,7 +157,7 @@ checkBan($mysqli);
                         </div>
                         <div class="member-info">
                             <h3 class="member-name">
-                                <a href="../marine_houshou">Marine Houshou</a>
+                                <a href="../user/marine_houshou">Marine Houshou</a>
                             </h3>
                             <p class="member-description">
                                 <strong>Cantarella</strong> è solo sua e <strong>Cipher</strong> l'ha già ingravidata, è un grande amante dei gacha e del gooning. Si è aggiudicato il primo posto in <a href="goonland/goon-generator">Goonland</a>
