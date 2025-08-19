bce89fd08b7f0f666d4a2332f7d9d056a31cb794
cripsum <galli.leonardo2005gmail.com>
2025-08-19 19:25:55 +0200

    Update inventory stylesheet link to include versioning for cache busting

--- it/inventario.php
+++ it/inventario.php
@@ -18,7 +18,7 @@ if (!isLoggedIn()) {
 <html lang="en">
     <head>
         <?php include '../includes/head-import.php'; ?>
-        <link rel="stylesheet" href="../css/inventario.css" />
+        <link rel="stylesheet" href="../css/inventario.css?v=1" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Cripsum™ - inventario</title>
     </head>
