fc71713395ec7065d4441ed648654c8200bd92d5
cripsum <galli.leonardo2005gmail.com>
2025-08-19 17:16:50 +0200

    Update achievement unlock condition in lootbox.php to require 43 items

--- it/lootbox.php
+++ it/lootbox.php
@@ -628,7 +628,7 @@ require_once '../api/api_personaggi.php';
                 if (inventory.length === 1) {
                     unlockAchievement(5);
                 }
-                if (inventory.length === 42) {
+                if (inventory.length === 43) {
                     unlockAchievement(18);
                 }
             }
