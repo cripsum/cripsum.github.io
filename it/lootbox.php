664926169d7bfccf16d1af6b523dc353cf9a6cfd
cripsum <galli.leonardo2005gmail.com>
2025-08-20 05:32:51 +0200

    Improve styling for rarity probability display in lootbox modal

--- it/lootbox.php
+++ it/lootbox.php
@@ -154,7 +154,7 @@ require_once '../api/api_personaggi.php';
                                 <!--<button type="button" class="btn-close tastobianco" data-bs-dismiss="modal" aria-label="Close" onclick="close_disclaimer(1)" style="color: #ffffff"></button>-->
                             </div>
                                 <div class="col-md-6 d-flex text-center" style="text-align: center; padding-top: 20px; padding-bottom: 20px; margin: auto;">
-                                    <div style="color: white; font-size: 14px;">
+                                    <div style="color: white; font-size: 14px; margin: auto;">
                                         <h6 style="color: white; margin-bottom: 10px;">Probabilità Rarità:</h6>
                                         <div>Comune: 45%</div>
                                         <div>Raro: 25%</div>
