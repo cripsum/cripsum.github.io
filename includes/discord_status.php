04ba1afbc4b04f8ebc6a247da3a3523eacc1ac94
cripsum <galli.leonardo2005gmail.com>
2025-08-25 18:26:49 +0200

    gay

--- includes/discord_status.php
+++ includes/discord_status.php
@@ -123,10 +123,10 @@ $data = getDiscordPresence($discord_id);
 
         .status-indicator {
             position: absolute;
-            bottom: -6px;
-            right: -6px;
-            width: 20px;
-            height: 20px;
+            bottom: -5px;
+            right: -5px;
+            width: 16px;
+            height: 16px;
             border-radius: 50%;
             border: 3px solid rgba(0, 0, 0, 0.8);
         }
