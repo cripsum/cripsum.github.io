a0e0e325d6bedb056a70f93994d5a711aeb734a4
cripsum <galli.leonardo2005gmail.com>
2025-08-27 04:27:07 +0200

    gay

--- api/update_activity.php
+++ api/update_activity.php
@@ -1,6 +1,12 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors',1);
+ini_set('log_errors', 1);
+error_reporting(1);
+
 session_start();
 if(!isset($_SESSION['user_id'])) exit;
+
 require_once __DIR__ . '/../config/database.php';
 require_once __DIR__ . '/../includes/functions.php';
 
