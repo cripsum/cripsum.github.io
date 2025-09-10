bdfc38956af605b5d31c6488cc34c144e650483b
cripsum <galli.leonardo2005gmail.com>
2025-09-10 14:24:51 +0200

    gay

--- it/animazione_personaggio.php
+++ it/animazione_personaggio.php
@@ -173,7 +173,7 @@ $idPersonaggio = $_GET['id_personaggio'] ?? 0;
                         bagliore.style.height = "100vh";
                         bagliore.style.zIndex = "-1";
 
-                    } else if (pull.rarita === "theone") {
+                    } else if (pull.rarità === "theone") {
 
                         startTheOneAnimation(pull.nome);
                         messaggioRarita.innerText = "INCREDBILE! HAI PULLATO IL PERSONAGGIO PIÙ RARO DI TUTTI!!!";
