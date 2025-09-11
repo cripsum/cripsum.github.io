76e5f9660cd04be4f5e24c67b709e4a3f799927b
cripsum <galli.leonardo2005gmail.com>
2025-09-11 20:16:08 +0200

    Refactor session management by moving configuration to session_init.php and updating home.php to require this file

---
+++ config/session_init.php
@@ -0,0 +1,15 @@
+<?php
+ini_set('session.gc_maxlifetime', 604800);
+ini_set('session.cookie_lifetime', 604800);
+ini_set('session.gc_probability', 1);
+ini_set('session.gc_divisor', 1000);
+session_set_cookie_params([
+    'lifetime' => 604800,
+    'path' => '/',
+    'domain' => '.cripsum.com',
+    'secure' => true,
+    'httponly' => true,
+    'samesite' => 'Lax'
+]);
+session_start();
+?>
