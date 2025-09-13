b7675f1fc02df7a571c583139178fe4d42c2cc1c
cripsum <galli.leonardo2005gmail.com>
2025-09-13 13:19:18 +0200

    Add initial uwu.php file with animated background and text

---
+++ uwu.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<html lang="it">
+<head>
+<meta charset="utf-8">
+<title>uwu</title>
+<style>
+  html, body {
+    margin: 0;
+    height: 100%;
+  }
+  body {
+    animation: flash 0.1s infinite;
+  }
+  @keyframes flash {
+    0%   { background: black; }
+    50%  { background: white; }
+    100% { background: black; }
+  }
+</style>
+</head>
+<body>
+    <div style="
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        font-size: 20em;
+        font-weight: bold;
+        animation: textFlash 0.1s infinite;
+    ">
+        UWU
+    </div>
+    <style>
+    @keyframes textFlash {
+        0%   { color: white; }
+        50%  { color: black; }
+        100% { color: white; }
+    }
+    </style>
+</body>
+</html>
