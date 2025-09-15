36a56455ddb9129f5fd7707ea28879dc2d3f5d2e
cripsum <galli.leonardo2005gmail.com>
2025-09-15 02:50:13 +0200

    Remove transform effect from edit card animation for smoother display

--- it/edits.php
+++ it/edits.php
@@ -1197,7 +1197,6 @@ checkBan($mysqli);
                                 card.style.display = 'block';
                                 setTimeout(() => {
                                     card.style.opacity = '1';
-                                    card.style.transform = 'translateY(0) scale(1)';
                                 }, visibleIndex * 100);
                                 visibleIndex++;
                             } else {
