7de3b1ca40351fe0ff8691d9a694665bf5179aa6
cripsum <galli.leonardo2005gmail.com>
2025-11-12 13:31:52 +0100

    Update achievement scripts to load only for logged-in users

--- includes/head-import.php
+++ includes/head-import.php
@@ -34,7 +34,7 @@
         <script src="/js/controlloLingua-it.js?v=2"></script>
         <script src="/js/controlloTema.js"></script>
 
-        <?php if (!$isLoggedIn): ?>
+        <?php if ($isLoggedIn): ?>
             <script src="/js/unlockAchievement-it.js?v=2"></script>
             <script src="/js/achievements-globali.js?v=3"></script>
         <?php endif; ?>
