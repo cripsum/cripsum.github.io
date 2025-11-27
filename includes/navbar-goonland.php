d44836f92a14cbea3d8f14aea0b8c2612506a3b6
cripsum <galli.leonardo2005gmail.com>
2025-11-27 12:22:21 +0100

    rimosso tasto impostazioni al momento inutile

--- includes/navbar-goonland.php
+++ includes/navbar-goonland.php
@@ -80,11 +80,11 @@ if ($isLoggedIn) {
             </ul>
         </div>
 
-        <div class="btn-group ms-auto me-3 linguanuova">
+        <!-- <div class="btn-group ms-auto me-3 linguanuova">
             <button type="button" class="btn impostazioni-toggler" data-bs-toggle="modal" data-bs-target="#impostazioniModal">
                 <img src="/img/settings-icon.svg" alt="Impostazioni" style="width: 25px" class="imgbianca" />
             </button>
-        </div>
+        </div> -->
     </div>
 </nav>
     <?php if ($richpresence === 1): ?>
--- includes/navbar.php
+++ includes/navbar.php
@@ -102,11 +102,11 @@ if ($isLoggedIn) {
             </ul>
         </div>
 
-        <div class="btn-group ms-auto me-3 linguanuova">
+        <!-- <div class="btn-group ms-auto me-3 linguanuova">
             <button type="button" class="btn impostazioni-toggler" data-bs-toggle="modal" data-bs-target="#impostazioniModal">
-                <img src="/img/settings-icon.svg" alt="Impostazioni" style="width: 25px" class="imgbianca impostazioni-toggler-icon" />
+                <img src="/img/settings-icon.svg" alt="Impostazioni" style="width: 25px" class="imgbianca impostazioni-toggler-icobn" />
             </button>
-        </div>
+        </div> -->
     </div>
 </nav>
     <?php if ($richpresence === 1): ?>
