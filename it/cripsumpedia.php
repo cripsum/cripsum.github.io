a86de1e76870bcdca897d91b87c8a580fea02d6c
cripsum <galli.leonardo2005gmail.com>
2025-12-15 21:02:31 +0100

    gay

---
+++ it/cripsumpedia.php
@@ -0,0 +1,41 @@
+<?php
+require_once '../config/session_init.php';
+require_once '../config/database.php';
+require_once '../includes/functions.php';
+checkBan($mysqli);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include '../includes/head-import.php'; ?>
+    <title>Cripsum™</title>
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1527058839538660"
+        crossorigin="anonymous"></script>
+</head>
+
+<body class="">
+    <?php include '../includes/navbar.php'; ?>
+    <?php include '../includes/impostazioni.php'; ?>
+
+    <div class="testobianco paginaprincipale">
+
+    </div>
+    <div id="achievement-popup" class="popup">
+        <img id="popup-image" src="" alt="Achievement" />
+        <div>
+            <h3 id="popup-title"></h3>
+            <p id="popup-description"></p>
+        </div>
+    </div>
+    <?php include '../includes/footer.php'; ?>
+    <script
+        src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+        crossorigin="anonymous"></script>
+
+    <script src="../js/modeChanger.js"></script>
+</body>
+
+</html>
