2b9aad8392535ca5e5575e933e7e2baf6a4915be
cripsum <galli.leonardo2005gmail.com>
2026-01-22 11:43:14 +0100

    gay

--- it/cripsumpedia/u/template.php
+++ it/cripsumpedia/u/template.php
@@ -4,8 +4,6 @@ require_once '../../config/database.php';
 require_once '../../includes/functions.php';
 checkBan($mysqli);
 
-$persona = $_GET['id'] ?? null;
-
 if (!isLoggedIn()) {
     $_SESSION['error_message'] = "mi dispiace, ma la pagina Cripsumpedia™ è ancora in fase di sviluppo.";
     header('Location: ../home');
@@ -19,14 +17,13 @@ if (!isOwner()) {
 
 ?>
 
-
 <!DOCTYPE html>
 <html lang="it">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php echo htmlspecialchars($persona); ?> - Cripsumpedia™</title>
+    <title> - Cripsumpedia™</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet">
