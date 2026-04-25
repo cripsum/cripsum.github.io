2977ed3fe6a8e1fad9571c6e8c83beae1d0d8eb8
cripsum <galli.leonardo2005gmail.com>
2026-04-26 01:20:12 +0200

    gayy

--- config/discord_oauth.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-define('CRIPSUM_DISCORD_CLIENT_ID', getenv('DISCORD_CLIENT_ID') ?: '1375776178568429631');
-define('CRIPSUM_DISCORD_CLIENT_SECRET', getenv('DISCORD_CLIENT_SECRET') ?: 'Og-lbm7s1pUrbedq0_yCk5w9EdFRSEAB');
-define('CRIPSUM_DISCORD_REDIRECT_URI', getenv('DISCORD_REDIRECT_URI') ?: 'https://cripsum.com/auth/discord_callback.php');
