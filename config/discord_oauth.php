67a8e309231d9acce032699909f0b33d647fcb17
cripsum <galli.leonardo2005gmail.com>
2026-04-26 01:17:14 +0200

    gay

--- config/discord_oauth.php
+++ config/discord_oauth.php
@@ -1,7 +1,5 @@
 <?php
-// Copia questo file e imposta questi valori nel server.
-// Il redirect URI deve essere identico a quello inserito nel Discord Developer Portal.
 
-define('CRIPSUM_DISCORD_CLIENT_ID', getenv('DISCORD_CLIENT_ID') ?: 'INSERISCI_CLIENT_ID');
-define('CRIPSUM_DISCORD_CLIENT_SECRET', getenv('DISCORD_CLIENT_SECRET') ?: 'INSERISCI_CLIENT_SECRET');
+define('CRIPSUM_DISCORD_CLIENT_ID', getenv('DISCORD_CLIENT_ID') ?: '1375776178568429631');
+define('CRIPSUM_DISCORD_CLIENT_SECRET', getenv('DISCORD_CLIENT_SECRET') ?: 'Og-lbm7s1pUrbedq0_yCk5w9EdFRSEAB');
 define('CRIPSUM_DISCORD_REDIRECT_URI', getenv('DISCORD_REDIRECT_URI') ?: 'https://cripsum.com/auth/discord_callback.php');
