b763d1d85dc5e1d3720e99fe5f6db8bb8a891c03
cripsum <galli.leonardo2005gmail.com>
2026-04-26 13:14:21 +0200

    asdfa

--- it/inventario.php
+++ it/inventario.php
@@ -41,8 +41,8 @@ $ogUrl = 'https://cripsum.com' . strtok((string)($_SERVER['REQUEST_URI'] ?? '/it
     <meta property="og:url" content="<?php echo inventario_h($ogUrl); ?>">
     <meta name="twitter:card" content="summary_large_image">
 
-    <link rel="stylesheet" href="/assets/inventario/inventario.css?v=2.71">
-    <script src="/assets/inventario/inventario.js?v=2.71" defer></script>
+    <link rel="stylesheet" href="/assets/inventario/inventario.css?v=2.8">
+    <script src="/assets/inventario/inventario.js?v=2.8" defer></script>
 </head>
 
 <body class="inv-page">
