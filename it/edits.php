ca16c9bd10162c6d0d60f51e9afff62e4adbb289
cripsum <galli.leonardo2005gmail.com>
2026-05-12 09:02:07 +0200

    update z-index values in edits.css and increment version in edits.php

--- it/edits.php
+++ it/edits.php
@@ -345,7 +345,7 @@ $totalEdits = count($edits);
     <?php include '../includes/head-import.php'; ?>
     <title>Cripsum™ - Edits</title>
     <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover">
-    <link rel="stylesheet" href="/css/edits.css?v=3.4">
+    <link rel="stylesheet" href="/css/edits.css?v=3.5">
     <script src="/js/edits.js?v=3.3" defer></script>
 </head>
 
