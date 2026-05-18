581cf587776f67b58c5fdfef0adb7675d6a60db6
cripsum <galli.leonardo2005gmail.com>
2026-05-18 04:02:35 +0200

    asdads

--- en/lootbox.php
+++ en/lootbox.php
@@ -65,7 +65,7 @@ defined('PITY_EVENTO_SOFT') || define('PITY_EVENTO_SOFT',   65);
 <head>
     <?php include '../includes/head-import.php'; ?>
     <link rel="stylesheet" href="/css/lootbox.css?v=8.2">
-    <link rel="stylesheet" href="/css/gacha.css?v=8">
+    <link rel="stylesheet" href="/css/gacha.css?v=9">
     <meta name="theme-color" content="#080810">
     <title>Cripsum™ — Lootbox</title>
 </head>
