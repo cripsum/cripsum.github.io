de1e4ad0c8d2fcd49f99988780deee2fcf1064a9
cripsum <galli.leonardo2005@gmail.com>
2026-06-08 05:13:59 +0200

    feat(database): Add initial database connection setup

---
+++ config/database.php
@@ -0,0 +1,11 @@
+<?php
+
+
+require_once __DIR__ . '/../secure/config.php';
+
+
+$mysqli = new mysqli($db_host, $db_user, $db_pass, $db_name);
+
+if ($mysqli->connect_error) {
+    die('Connection failed: ' . $mysqli->connect_error);
+}
