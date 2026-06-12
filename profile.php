99d15fe6ee0bcbe66e45c4e866956bead8e5edb9
cripsum <galli.leonardo2005@gmail.com>
2026-06-12 03:36:37 +0200

    feat: Update profile CSS/JS version to 4.2.6 and enhance avatar styles

--- profile.php
+++ profile.php
@@ -347,8 +347,8 @@ if (isset($_SESSION['lang']) && $_SESSION['lang'] === 'en') {
     <title><?php echo $profile ? 'Cripsum™ - ' . profile_h($displayName) : 'Cripsum™ - Profilo'; ?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php cripsum_og_print($ogMeta); ?>
-    <link rel="stylesheet" href="/assets/css/profile.css?v=4.2.5">
-    <script src="/assets/js/profile.js?v=4.2.5" defer></script>
+    <link rel="stylesheet" href="/assets/css/profile.css?v=4.2.6">
+    <script src="/assets/js/profile.js?v=4.2.6" defer></script>
     <?php
     $googleFonts = [
         'Poppins' => 'Poppins',
